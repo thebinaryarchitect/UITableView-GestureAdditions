c2098524cfc3ec26340f4c348586e0eec823ffd4
thebinaryarchitect <thebinaryarchitect@gmail.com>
2015-08-23 14:27:39 -0400

    Initial Commit

---
+++ UITableView+GestureAdditions/UITableView+GestureAdditions.h
@@ -0,0 +1,19 @@
+//
+//  UITableView+GestureAdditions.h
+//  UITableView+GestureAdditions
+//
+//  Created by Xiao Yao on 8/23/15.
+//  Copyright (c) 2015 Xiao Yao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UITableView+GestureAdditions.
+FOUNDATION_EXPORT double UITableView_GestureAdditionsVersionNumber;
+
+//! Project version string for UITableView+GestureAdditions.
+FOUNDATION_EXPORT const unsigned char UITableView_GestureAdditionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UITableView_GestureAdditions/PublicHeader.h>
+
+
