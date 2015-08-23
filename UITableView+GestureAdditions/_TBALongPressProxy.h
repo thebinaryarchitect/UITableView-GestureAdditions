fd77e69c2e1347bb477e2996776acfd5b6ef7a3b
thebinaryarchitect <thebinaryarchitect@gmail.com>
2015-08-23 15:03:53 -0400

    Implement reorder functionality

--- UITableView+GestureAdditions/_TBALongPressProxy.h
+++ UITableView+GestureAdditions/_TBALongPressProxy.h
@@ -34,6 +34,31 @@
 @property (nonatomic, strong, readwrite) UILongPressGestureRecognizer *longPressGestureRecognizer;
 
 /**
+ *  The selected table view cell.
+ */
+@property (nonatomic, strong, readwrite) UITableViewCell *selectedTableViewCell;
+
+/**
+ *  The snapshot of the cell being reordered.
+ */
+@property (nonatomic, strong, readwrite) UIImageView *snapshot;
+
+/**
+ *  The display link used to auto scroll.
+ */
+@property (nonatomic, strong, readwrite) CADisplayLink *displayLink;
+
+/**
+ *  The speed of the auto scroll.
+ */
+@property (nonatomic, assign, readwrite) CGFloat scrollRate;
+
+/**
+ *  The index path of the selected cell at the beginning of the gesture.
+ */
+@property (nonatomic, assign, readwrite) NSIndexPath *sourceIndexPath;
+
+/**
  *  The designated initializer.
  *
  *  @param tableView The table view.
--- UITableView+GestureAdditions/_TBALongPressProxy.m
+++ UITableView+GestureAdditions/_TBALongPressProxy.m
@@ -43,6 +43,139 @@
 #pragma mark Private
 
 - (void)handleLongPress:(UILongPressGestureRecognizer *)recognizer {
+    CGPoint touchLocation = [recognizer locationInView:recognizer.view];
+    NSIndexPath *indexPath = [self.tableView indexPathForRowAtPoint:touchLocation];
+    switch (recognizer.state) {
+        case UIGestureRecognizerStateBegan: {
+            UITableViewCell *cell = [self.tableView cellForRowAtIndexPath:indexPath];
+            cell.backgroundColor = [UIColor whiteColor];
+            [cell setHighlighted:NO];
+            [cell setSelected:NO];
+            self.selectedTableViewCell = cell;
+            self.sourceIndexPath = indexPath;
+            
+            UIGraphicsBeginImageContextWithOptions(cell.bounds.size, NO, 0.0);
+            [cell.layer renderInContext:UIGraphicsGetCurrentContext()];
+            UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+            UIGraphicsEndImageContext();
+            
+            UIImageView *snapshot = [[UIImageView alloc] initWithImage:image];
+            CALayer *layer = snapshot.layer;
+            layer.shadowColor = [UIColor blackColor].CGColor;
+            layer.shadowOffset = CGSizeMake(0.0, 0.0);
+            layer.shadowOpacity = 0.25;
+            layer.shadowRadius = 5.0;
+            CGRect frame = cell.frame;
+            frame.origin.x = 0.0;
+            snapshot.frame = frame;
+            snapshot.alpha = 0.0;
+            [self.tableView addSubview:snapshot];
+            self.snapshot = snapshot;
+            
+            [UIView animateWithDuration:0.25 animations:^{
+                self.snapshot.transform = CGAffineTransformMakeScale(1.025, 1.025);
+                
+                self.snapshot.alpha = 1.0;
+                
+                CGPoint center = self.snapshot.center;
+                center.y = touchLocation.y;
+                self.snapshot.center = center;
+                
+                self.selectedTableViewCell.hidden = YES;
+            }];
+            
+            CADisplayLink *link = [CADisplayLink displayLinkWithTarget:self selector:@selector(tick:)];
+            [link addToRunLoop:[NSRunLoop mainRunLoop] forMode:NSDefaultRunLoopMode];
+            self.displayLink = link;
+            break;
+        }
+        case UIGestureRecognizerStateChanged: {
+            // Calculate scroll rate
+            CGRect rect = self.tableView.bounds;
+            UIEdgeInsets contentInset = self.tableView.contentInset;
+            rect.size.height -= contentInset.top;
+            CGFloat scrollZoneHeight = MIN(self.selectedTableViewCell.frame.size.height * 2.0, rect.size.height / 5.0);
+            CGPoint contentOffset = self.tableView.contentOffset;
+            CGFloat minY = contentOffset.y + contentInset.top + scrollZoneHeight;
+            CGFloat maxY = contentOffset.y + contentInset.top +rect.size.height - scrollZoneHeight;
+            if (touchLocation.y >= maxY) {
+                self.scrollRate = (touchLocation.y - maxY) / scrollZoneHeight;
+            } else if (touchLocation.y <= minY) {
+                self.scrollRate = (touchLocation.y - minY) / scrollZoneHeight;
+            } else {
+                self.scrollRate = 0.0;
+                [self updateCell];
+            }
+            break;
+        }
+        default: {
+            self.scrollRate = 0.0;
+            [self.displayLink invalidate];
+            self.displayLink = nil;
+            
+            CGRect frame =  self.selectedTableViewCell.frame;
+            
+            [UIView animateWithDuration:0.25 animations:^{
+                self.snapshot.transform = CGAffineTransformMakeScale(1.0, 1.0);
+                self.snapshot.frame = frame;
+                self.snapshot.alpha = 0.0;
+            } completion:^(BOOL finished) {
+                [self.snapshot removeFromSuperview];
+                self.snapshot = nil;
+                self.selectedTableViewCell.hidden = NO;
+                self.selectedTableViewCell = nil;
+                self.sourceIndexPath = nil;
+            }];
+            break;
+        }
+    }
+}
+
+- (void)updateCell {
+    CGPoint touchLocation = [self.longPressGestureRecognizer locationInView:self.longPressGestureRecognizer.view];
+    NSIndexPath *selectedIndexPath = [self.tableView indexPathForCell:self.selectedTableViewCell];
+    NSIndexPath *indexPath = [self.tableView indexPathForRowAtPoint:touchLocation];
+    if (![selectedIndexPath isEqual:indexPath] && indexPath) {
+        if ([self.tableView.dataSource respondsToSelector:@selector(tableView:moveRowAtIndexPath:toIndexPath:)]) {
+            [self.tableView.dataSource tableView:self.tableView moveRowAtIndexPath:selectedIndexPath toIndexPath:indexPath];
+        }
+        [self.tableView moveRowAtIndexPath:selectedIndexPath toIndexPath:indexPath];
+    }
+    
+    // Update snapshot center
+    CGPoint center = self.snapshot.center;
+    touchLocation.y = MAX(0.0, touchLocation.y);
+    touchLocation.y = MIN(self.tableView.contentSize.height, touchLocation.y);
+    center.y = touchLocation.y;
+    self.snapshot.center = center;
+}
+
+- (void)tick:(CADisplayLink *)link {
+    if (self.scrollRate != 0.0) {
+        // Update content offset.
+        CGPoint offset = self.tableView.contentOffset;
+        offset.y += 15.0 * self.scrollRate;
+        
+        UIEdgeInsets contentInset = self.tableView.contentInset;
+        CGFloat top = contentInset.top;
+        if (offset.y < -top) {
+            offset.y = -top;
+        } else {
+            CGSize contentSize = self.tableView.contentSize;
+            CGRect frame = self.tableView.frame;
+            if (contentSize.height + contentInset.bottom < frame.size.height) {
+                offset = self.tableView.contentOffset;
+            } else {
+                CGFloat max = contentSize.height + contentInset.bottom - frame.size.height;
+                if (offset.y > max) {
+                    offset.y = max;
+                }
+            }
+        }
+        
+        self.tableView.contentOffset = offset;
+        [self updateCell];
+    }
 }
 
 #pragma mark UIGestureRecognizerDelegate
